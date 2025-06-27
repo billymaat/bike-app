4aa798b2effcfb45e7a712f7cca4c2a6e7c2add2
Billy Maat <bsmaat@gmail.com>
2025-06-27 23:54:53 +0100

    backup

--- src/app/pages/edit-event/edit-event.page.ts
+++ src/app/pages/edit-event/edit-event.page.ts
@@ -9,6 +9,7 @@ import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatInputModule } from '@angular/material/input';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
+import { CycleEvent } from '../../models/cycle-event';
 
 @Component({
   selector: 'app-edit-event',
@@ -22,18 +23,20 @@ import { MatButtonModule } from '@angular/material/button';
   ],
   providers: [provideNativeDateAdapter()],
   templateUrl: './edit-event.page.html',
-  styleUrl: './edit-event.page.scss'
+  styleUrl: './edit-event.page.scss'  
 })
 export class EditEventComponent {
 
   public dialogRef = inject(MatDialogRef<EditEventComponent>);
   public data = inject(MAT_DIALOG_DATA);
 
+  private originalEvent: CycleEvent | null = null;
   store = inject(CycleEventStore);
   private activatedRoute = inject(ActivatedRoute);
   eventId = signal<number>(0);
   
   eventForm = new FormGroup({
+    id: new FormControl(0),
     name: new FormControl(''),
     description: new FormControl(''),
     date: new FormControl(''),
@@ -54,7 +57,13 @@ export class EditEventComponent {
       console.log('Event ID changed:', this.eventId());
       const event = this.store.getCycleEvent(this.eventId());
       if (event) {
+        if (!this.originalEvent)
+        {
+          this.originalEvent = { ...event }; // Store the original event
+        }
+        
         this.eventForm.setValue({
+          id: event.id,
           name: event.name,
           description: event.description,
           date: event.date ? event.date.toISOString().split('T')[0] : '',
@@ -76,5 +85,26 @@ export class EditEventComponent {
     this.store.updateCycleEvent(updatedEvent);
     this.dialogRef.close(updatedEvent);
     console.log('Event updated:', updatedEvent);
+  }
+
+  isFormChanged(): boolean {
+      if (!this.originalEvent) return false;
+
+    // Normalize date to YYYY-MM-DD string for both
+    const originalDate = this.originalEvent.date
+      ? new Date(this.originalEvent.date).toISOString().split('T')[0]
+      : '';
+    const selectedEventDate: Date = this.eventForm.value.date ? new Date(this.eventForm.value.date) : new Date();
+
+    const formDate = selectedEventDate.toISOString().split('T')[0];
+
+    // Compare all fields
+    const isChanged =
+      (this.eventForm.value.name || '') !== (this.originalEvent.name || '') ||
+      (this.eventForm.value.description || '') !== (this.originalEvent.description || '') ||
+      (this.eventForm.value.location || '') !== (this.originalEvent.location || '') ||
+      formDate !== originalDate;
+
+    return isChanged;
   }
 }
