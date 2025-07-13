aecb1aa91983c4182ed14b22f144deb1fea76563
Billy Maat <bsmaat@gmail.com>
2025-07-13 23:27:46 +0100

    add sort

--- src/app/pages/users/users.page.ts
+++ src/app/pages/users/users.page.ts
@@ -1,23 +1,59 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild, AfterViewInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableModule } from '@angular/material/table';
 import { UserStore } from './users.store';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Sort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-users',
   imports: [MatCardModule,
     MatTableModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSortModule
   ],
   providers: [UserStore],
   templateUrl: './users.page.html',
   styleUrl: './users.page.scss'
 })
-export class UsersPage {
+export class UsersPage implements AfterViewInit {
 
   store = inject(UserStore);
+
+  dataSource = new MatTableDataSource(this.store.users());
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+  sortData(sort: Sort) {
+    const data = this.store.users().slice();
+    if (!sort.active || sort.direction === '') {
+      this.dataSource.data = data;
+      return;
+    }
+
+    this.dataSource.data = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'firstName':
+          return compare(a.firstName, b.firstName, isAsc);
+        case 'lastName':
+          return compare(a.lastName, b.lastName, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+
   viewUser(userId: number) {
     // Logic to view user details
   }
+}
+
+function compare(a: string, b: string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
